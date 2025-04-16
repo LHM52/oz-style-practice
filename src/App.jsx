42ba0c289435f4614c99977c6e328f404731cb39
LHM52 <dlgudals8410@naver.com>
2025-04-16 22:58:22 +0900

    11일차 과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,41 @@
-import "./App.scss";
+// import "./App.scss";
 import { contents } from "./assets/data/data";
 import Content from "./components/Content";
 import Header from "./components/Header";
+import styled, { css } from 'styled-components';
+
+
+
+const flexMixin = ({
+  justify = 'flex-start',
+  align = 'stretch',
+  gap = '0',
+  wrap = 'nowrap'
+  
+}) => css`
+  display: flex;
+  justify-content: ${justify};
+  align-items: ${align};
+  gap: ${gap};
+  flex-wrap: ${wrap};
+`;
+
+
+
+let StyledSection = styled.section`
+${flexMixin({ justify: 'center', align: 'center', gap: '20px', wrap: 'wrap' })};
+  padding: 20px 40px;
+`;
 
 function App() {
   return (
     <main>
       <Header />
-      <section>
+      <StyledSection>
         {contents.map((el) => (
           <Content key={el.id} content={el} />
         ))}
-      </section>
+      </StyledSection>
     </main>
   );
 }
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,10 +1,70 @@
+import styled, { css } from 'styled-components';
+
+let tagColor = '#d7fa00';
+let gray = 'rgb(160, 160, 160)';
+
+
+const flexMixin = ({
+  justify = 'flex-start',
+  align = 'stretch',
+  gap = '0',
+  wrap = 'nowrap',
+  direction = 'row'
+}) => css`
+  display: flex;
+  justify-content: ${justify};
+  align-items: ${align};
+  gap: ${gap};
+  flex-wrap: ${wrap};
+  flex-direction: ${direction};
+`;
+
+const fontMixin = ({
+  size,
+  weight = 400
+}) => css`
+  font-size: ${size};
+  font-weight: ${weight};
+`
+
+const ContentContainer = styled.div`
+  ${flexMixin({
+  direction: 'column',
+  align: 'flex-start',
+  gap: '5px'
+})};
+  
+  img {
+    width: 300px;
+    border-radius: 10px;
+    margin-bottom: 3px;
+    min-height: 225px;
+  }
+  span {
+    ${fontMixin({ size: '12px' })};
+    color: ${tagColor};
+    border: 1px solid ${tagColor};
+    padding: 4px 5px;
+    border-radius: 3px;
+  }
+  div {
+    ${fontMixin({ size: '18px', $weight: 600 })};
+  }
+  p {
+    ${fontMixin({ $size: '12px' })};
+    color: ${gray};
+  }
+`
+
 export default function Content({ content }) {
   return (
-    <div className="content-container">
+
+    <ContentContainer>
       <img src={content.img} alt={content.title} />
       <span>모집중</span>
       <div>{content.title}</div>
       <p>{content.subtitle}</p>
-    </div>
+    </ContentContainer>
+
   );
 }
